5390a7bc2a6510260fb96a98faca213c8d1db6a3
MINK <76023265+kk-min@users.noreply.github.com>
2023-06-28 13:29:13 +0800

    Add flag for snapstart enabled/disabled

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ var burstCount = flag.Int("b", 1, "Number of bursts to send (default: 1)")
 var burstSize = flag.Int("s", 1, "Number of requests to send in a burst (default: 1)")
 var burstIAT = flag.Int("i", 10000, "Inter-arrival time between bursts in milliseconds (default: 10000)")
 var parallel = flag.Bool("p", false, "Send requests in parallel (default: false)")
+var snapStartEnabled = flag.Bool("snapstart" false, "Enable SnapStart (default: false)")
 
 func main() {
 	log.Info("Starting application...")
@@ -48,7 +49,13 @@ func main() {
 			"incrementLimit": 1,
 		},
 	}
-	req, _ := http.NewRequest(http.MethodGet, "<API_GATEWAY_ROUTE>hellojava_SnapStartDisabled", reqBody)
+	endpoint := "<API_GATEWAY_ROUTE>"
+	if snapStartEnabled {
+		endpoint = endpoint + "hellojava_SnapStartEnabled"
+	}else {
+		endpoint = endpoint + "hellojava_SnapStartDisabled"
+	}
+	req, _ := http.NewRequest(http.MethodGet, endpoint, reqBody)
 	req = req.WithContext(ctx)
 	signer := v4.NewSigner(cfg.Credentials)
 	_, err = signer.Sign(req, nil, "execute-api", cfg.Region, time.Now())
