507a2703abc77bf0ffd07814f53fde8afe1edb6f
MINK <76023265+kk-min@users.noreply.github.com>
2023-06-28 13:09:30 +0800

    Add request creation and sending

--- main.go
+++ main.go
@@ -24,11 +24,39 @@ func main() {
 	log.Info("Starting application...")
 	flag.Parse()
 
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	credentials, err := cfg.Credentials.Retrieve(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
 	outputDirPath := filepath.Join(*outputDir, time.Now().Format(time.RFC850))
 	log.Infof("Creating dir for this benchmark at `%s`", outputDirPath)
 	if err := os.MkdirAll(outputDirPath, os.ModePerm); err != nil {
 		log.Fatal(err)
 	}
 
-	log.Info("Creating API Gateway client...")
+	req, _ := http.NewRequest(http.MethodGet, "", nil)
+	req = req.WithContext(ctx)
+	signer := v4.NewSigner(cfg.Credentials)
+	_, err = signer.Sign(req, nil, "execute-api", cfg.Region, time.Now())
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatal(err)
+		return
+	}
 }
