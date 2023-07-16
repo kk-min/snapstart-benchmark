411c6d73ffa1aa35f1b77adad87c36844ff70930
Min <76023265+kk-min@users.noreply.github.com>
2023-07-17 06:23:05 +0800

    Update main.go

--- main.go
+++ main.go
@@ -20,10 +20,7 @@ type RequestBody struct {
 
 var outputDir = flag.String("o", "./latency_samples/", "Output directory for latency samples (default: ./latency_samples/)")
 var burstCount = flag.Int("b", 1, "Number of bursts to send (default: 1)")
-var burstSize = flag.Int("s", 1, "Number of requests to send in a burst (default: 1)")
 var burstIAT = flag.Int("i", 10000, "Inter-arrival time between bursts in milliseconds (default: 10000)")
-var parallel = flag.Bool("p", false, "Send requests in parallel (default: false)")
-var snapStartEnabled = flag.Bool("snapstart", false, "Enable SnapStart (default: false)")
 
 func main() {
 	log.Info("Starting application...")
